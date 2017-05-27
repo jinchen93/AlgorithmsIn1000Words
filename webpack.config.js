ae66647370eed3bd6723765bbb313fe7f0059dd8
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-27 10:30:17 -0700

    Setup SASS stylings

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './lib/main.js',
+	  devtool: 'source-map',
+    output: {
+        filename: './algorithms_in_1000_words.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/,
+          loaders: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
+    }
+};
