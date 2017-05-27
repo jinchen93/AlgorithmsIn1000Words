b2b1e76d97e6aba519fe0c94d18c39a8cea101ad
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-27 08:28:31 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const BinaryTree = require("../Inheritance/binaryTree").BinaryTree;
+const BinaryNode = require("../Inheritance/binaryTree").BstNode;
+const Vector = require("./vector");
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+var canvas = d3.select("svg");
+
+const myTree = new BinaryTree();
+
+const insertionBalanced = [6, 4, 8, 3, 5, 1, 2, 0, 7, 9, 4.5, 5.5, 3.4, 4.6];
+const insertionMin = [0,1,2,3,4,5,6,7,8,9];
+const insertionMax = [9,8,7,6,5,4,3,2,1,0];
+for (var i = 0; i < insertionBalanced.length; i++) {
+  myTree.insert(insertionBalanced[i]);
+}
+
+const GRID = 50;
+class TreeViewer {
+  constructor(tree) {
+    this.canvas = d3.select("svg");
+    this.tree = tree;
+    this.width = canvas.attr("width");
+    this.height = canvas.attr("height");
+    this.root = d3.select("svg").append("g").attr("transform", `translate(${this.width/2},${this.height/2})`);
+  }
+
+  // start at end of array to idx 0. from the root go left if -1, right if 1.
+  _getPosition(node) {
+    let pos = new Vector();
+    let answers = [];
+    for(let parent = node._parent, curr = node; parent != null; curr = parent, parent = parent._parent) {
+      answers.push(parent._right === curr ? 1 : -1);
+    }
+    console.log(answers);
+  }
+}
+
+const myViewer = new TreeViewer(myTree);
+
+window.tree = myTree;
+window.viewer = myViewer;
---
+++ vector.js
@@ -0,0 +1,54 @@
+
+
+class Vector {
+  constructor (x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add (other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+
+  subtract (other) {
+    return new Vector(this.x - other.x, this.y - other.y);
+  }
+
+  times (num) {
+    return new Vector(this.x * num, this.y * num);
+  }
+
+  divide (num) {
+    return new Vector(this.x / num, this.y / num);
+  }
+
+  distanceSq () {
+    return this.x * this.x + this.y * this.y;
+  }
+
+  distance () {
+    return Math.sqrt(this.distanceSq());
+  }
+
+  normal() {
+    const dist = this.distance();
+    if(dist === 0)
+      return new Vector();
+    return this.divide(dist);
+  }
+
+  leftHandNormal() {
+    return new Vector(this.y, -this.x);
+  }
+
+  rightHandNormal() {
+    return new Vector(-this.y, this.x);
+  }
+
+  toString() {
+    return `{${this.x},${this.y}}`;
+  }
+}
+
+
+module.exports = Vector;
